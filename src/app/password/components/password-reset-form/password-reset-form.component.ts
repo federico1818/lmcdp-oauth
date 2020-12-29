f6ec63abbfa0e40351308fdcdb7e8370e7c04db3
Federico González <federico1818@gmail.com>
2020-12-29 19:53:16 -0300

    add utils library and validate password confirmed

--- src/app/password/components/password-reset-form/password-reset-form.component.ts
+++ src/app/password/components/password-reset-form/password-reset-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { Validators } from '@angular/forms'
+import { FormComponent } from '@federico1818/utils'
+
+import { confirmed } from 'src/app/shared/validators/confirmed'
 
 @Component({
-  selector: 'app-password-reset-form',
-  templateUrl: './password-reset-form.component.html',
-  styleUrls: ['./password-reset-form.component.scss']
+    selector: 'app-password-reset-form',
+    templateUrl: './password-reset-form.component.html',
+    styleUrls: ['./password-reset-form.component.scss']
 })
-export class PasswordResetFormComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class PasswordResetFormComponent extends FormComponent {
+    
+    public form = this.fb.group({
+        password: ['', [Validators.required, Validators.minLength(4)]],
+        password_confirmation: ['', [Validators.required, Validators.minLength(4)]]
+    }, {
+        validator: confirmed('password')
+    })
 
 }
