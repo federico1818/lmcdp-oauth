e9240ef7d65d639726d3db3d324036437b2f2cb5
Federico González <federico1818@gmail.com>
2020-12-30 12:33:22 -0300

    login form extends form component

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core'
-import { FormBuilder, Validators } from '@angular/forms'
-import { HttpErrorResponse } from '@angular/common/http'
-
-import { OauthService } from '@federico1818/passport'
-import { throwError } from 'rxjs'
-import { catchError } from 'rxjs/operators'
+import { Validators } from '@angular/forms'
+import { FormComponent } from '@federico1818/utils'
 
 @Component({
     selector: 'app-login-form',
@@ -12,37 +8,11 @@ import { catchError } from 'rxjs/operators'
     styleUrls: ['./login-form.component.scss']
 })
 
-export class LoginFormComponent {
-
-    public invalid_grant: boolean = false 
+export class LoginFormComponent extends FormComponent {
 
     public form = this.fb.group({
         email: ['', [Validators.required, Validators.email]],
         password: ['', [Validators.required]]
     })
-
-    constructor(
-        protected fb: FormBuilder,
-        protected oauthService: OauthService
-    ) {}
-
-    public onSubmit(): void {
-        if(this.form.valid)
-            this.login()
-    }
-
-    public login(): void {
-        this.oauthService.login(this.form.value).pipe(
-            catchError(this.handleError.bind(this))
-        ).subscribe((res: any) => {
-            this.invalid_grant = false
-        })
-    }
-
-    private handleError(error: HttpErrorResponse) {
-        if(error.error['error'] == 'invalid_grant')
-            this.invalid_grant = true
-        return throwError(error.error)
-    }
 
 }
--- src/app/login/pages/login/login.component.ts
+++ src/app/login/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { OauthService } from '@federico1818/passport'
+import { catchError } from 'rxjs/operators'
 
 @Component({
     selector: 'app-login',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core'
 })
 
 export class LoginComponent {
+    constructor(
+        protected oauthService: OauthService
+    ) {}
+    
+    public onSubmit(form: any): void {
+        this.login(form)
+    }
+
+    public login(form: any): void {
+        this.oauthService.login(form).subscribe()
+    }
 
 }
